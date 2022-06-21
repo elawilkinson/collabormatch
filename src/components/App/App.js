388302ad887cd79cf05a3ebf92f6115d3b5a7baa
Mabrouka Yaqoob <mabroukayaqoob@outlook.com>
2022-06-21 11:11:46 +0100

    H2 added, needs styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,9 @@ function App() {
      {/* NB navbar - logo, profile icon, hamburger icon -could go here if time */}
       <div className="intro">
         <h1>collabor8</h1>
-        <h2>"intro sentence about who we are"</h2>
+        <h2>Welcome to Collaborm8! </h2>
+        <p>Collaborm8 is a collaborative platform built on a world of possibility. The possibility of forming professional connections that could lead to expanding your portfolio and widening your coding experience.</p>
+        <p>Collaborm8 is simple and fun- click the 'YAAS'™ feature to collaborate with someone, and if they wish to collaborate too, it's a CollaborMatch™ ! </p>
        </div>
       <div className="user-input">
          <p>Search below for your perfect collaborm8</p>
