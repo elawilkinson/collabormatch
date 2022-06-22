30454218433ed0421d959556927c89fdadd0f80c
rebeccatuffnell <rctuffnell@gmail.com>
2022-06-22 12:08:01 +0100

    profile card styling refactored

--- src/components/ProfileCard/index.js
+++ src/components/ProfileCard/index.js
@@ -4,8 +4,10 @@ import "./index.css";
 export default function ProfileCard() {
   return (
     <div className="card">
-      <img src="soccharacteredit.JPG" alt="SoC character" />
-      <h3>Rebecca Tuffnell</h3>
+    <div className="image-container">
+      <img src="https://upload.wikimedia.org/wikipedia/en/8/8c/Cocktail_1988.jpg" alt="SoC character" />
+    </div>
+      <h3>Collyn</h3>
       <p className="project-interest">Project interest: Sports</p>
       <p className="project-type">Project Type: To-do list</p>
       <p className="strength">Strength: Frontend</p>
