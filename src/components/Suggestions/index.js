0b9c6ceda67553f766b4cbf3428d3f587fd5dc06
RafaPach <rafael_eli_pacheco@hotmail.com>
2022-06-23 10:45:35 +0100

    Added three more inputs

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React from 'react';
 
-function Suggestions( ) {
+function Suggestions() {
   return (
     <div className="suggestions-box">
-      <h3 id="suggestions-intro">Can't find a project you're interested in? Suggest one below!</h3>
+      <h3 id="suggestions-intro">
+        Can't find a project you're interested in? Suggest one below!
+      </h3>
       <form>
         <div className="suggestions-input">
           <label value="name">Your name</label>
           <input type="text"></input>
-          </div>
+        </div>
         <div className="suggestions-input">
           <label value="email">Your email</label>
           <input type="text"></input>
         </div>
         <div className="suggestions-input">
-          <label value="project_interest">Project suggestion</label>
+          <label value="project_interest">Project Interest</label>
+          <input type="text"></input>
+        </div>
+        <div className="suggestions-input">
+          <label value="name">Project Type</label>
+          <input type="text"></input>
+        </div>
+        <div className="suggestions-input">
+          <label value="name">Availablity</label>
+          <input type="text"></input>
+        </div>
+        <div className="suggestions-input">
+          <label value="name">Strength</label>
           <input type="text"></input>
         </div>
       </form>
       <button className="suggestions-button">Send suggestion</button>
-      </div>
+    </div>
   );
 }
 
